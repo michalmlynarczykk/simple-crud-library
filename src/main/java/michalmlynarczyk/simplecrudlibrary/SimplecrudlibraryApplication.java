e40368ce54e9e91e69360fe416069ab0bcb80724
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-02 19:12:13 +0100

    initial comit

---
+++ src/main/java/michalmlynarczyk/simplecrudlibrary/SimplecrudlibraryApplication.java
@@ -0,0 +1,13 @@
+package michalmlynarczyk.simplecrudlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecrudlibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecrudlibraryApplication.class, args);
+	}
+
+}
