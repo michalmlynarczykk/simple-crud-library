1d050b01fb9c1a44e88538fd026c940d4859913a
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-04 11:17:18 +0100

    Fix: change return type from optional to list because of problems with verification when list is empty.

--- src/main/java/michalmlynarczyk/simplecrudlibrary/book/BookRepository.java
+++ src/main/java/michalmlynarczyk/simplecrudlibrary/book/BookRepository.java
@@ -1,10 +1,11 @@
 package michalmlynarczyk.simplecrudlibrary.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
-    Optional<List<Book>> findByAuthorFirstNameAndAuthorLastName(String firstName, String lastName);
+    List<Book> findByAuthorFirstNameAndAuthorLastName(String firstName, String lastName);
 }
