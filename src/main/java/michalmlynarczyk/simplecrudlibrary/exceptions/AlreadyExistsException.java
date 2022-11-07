ba82e39aae9e8188705f69bfde94dd2b3328afbc
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-07 14:42:51 +0100

    fix remove unused exception class.

--- src/main/java/michalmlynarczyk/simplecrudlibrary/exceptions/AlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package michalmlynarczyk.simplecrudlibrary.exceptions;
-
-public class AlreadyExistsException extends RuntimeException {
-    public AlreadyExistsException(String message) {
-        super(message);
-    }
-}
