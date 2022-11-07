5cebd4777501f255aa6c4aad71358338b779d79a
Michal Mlynarczyk <michalmlynarczykk@gmail.com>
2022-11-07 14:42:02 +0100

    add unique constraint to role entity on name field.

--- src/main/java/michalmlynarczyk/simplecrudlibrary/role/Role.java
+++ src/main/java/michalmlynarczyk/simplecrudlibrary/role/Role.java
@@ -1,15 +1,13 @@
 package michalmlynarczyk.simplecrudlibrary.role;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String name;
 
     public Role() {
